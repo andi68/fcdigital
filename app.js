c5afdfd69a8efd94a14f0fb91bf994b1d3627675
Jan <spallek.kj@gmail.com>
2016-12-07 10:38:01 +0100

    content

--- app.js
+++ app.js
@@ -97,11 +97,11 @@ function calculateOffers(_country, _travellerAges, _cover) {
 
     if (isChildCare(_travellerAges)) {
         offersChildCare = calculateOfferChildCare();
-        offers.push(offersChildCare);
+        offers.concat(offersChildCare);
     }
 
     var basics = ["hotel", "car insurance", "premier"];
-    basics.push(offers);
+    basics.concat(offers);
     return basics;
 }
 
